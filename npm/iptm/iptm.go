c2895eab32f758fb1d36be0e9cb0a48114133c64
Hunter Gregory <hunterlgregory@gmail.com>
2021-11-08 20:42:35 -0800

    added missing module args for ctstate NEW

--- npm/iptm/iptm.go
+++ npm/iptm/iptm.go
@@ -100,6 +100,8 @@ func (iptMgr *IptablesManager) UninitNpmChains() error {
 		Specs: []string{
 			util.IptablesJumpFlag,
 			util.IptablesAzureChain,
+			util.IptablesModuleFlag,
+			util.IptablesCtstateModuleFlag,
 			util.IptablesCtstateFlag,
 			util.IptablesNewState,
 		},
@@ -205,6 +207,8 @@ func (iptMgr *IptablesManager) checkAndAddForwardChain() error {
 		Specs: []string{
 			util.IptablesJumpFlag,
 			util.IptablesAzureChain,
+			util.IptablesModuleFlag,
+			util.IptablesCtstateModuleFlag,
 			util.IptablesCtstateFlag,
 			util.IptablesNewState,
 		},
