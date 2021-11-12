e56d03862d3143d1f7416bea280dccb9389f3f9f
Vamsi Kalapala <vkalapa@ncsu.edu>
2021-11-11 16:58:58 -0800

    Fixing a build issue in linux

--- npm/pkg/dataplane/ipsets/testutils_windows.go
+++ npm/pkg/dataplane/ipsets/testutils_windows.go
@@ -1,6 +1,22 @@
 package ipsets
 
-import testutils "github.com/Azure/azure-container-networking/test/utils"
+import (
+	"github.com/Azure/azure-container-networking/network/hnswrapper"
+	testutils "github.com/Azure/azure-container-networking/test/utils"
+	"github.com/Microsoft/hcsshim/hcn"
+)
+
+func GetHNSFake() *hnswrapper.Hnsv2wrapperFake {
+	hns := hnswrapper.NewHnsv2wrapperFake()
+	network := &hcn.HostComputeNetwork{
+		Id:   "1234",
+		Name: "azure",
+	}
+
+	hns.CreateNetwork(network)
+
+	return hns
+}
 
 func GetApplyIPSetsTestCalls(_, _ []*IPSetMetadata) []testutils.TestCmd {
 	return []testutils.TestCmd{}
