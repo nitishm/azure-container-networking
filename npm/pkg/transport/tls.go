918232ce8d7f7777d523ecd8f0d278ac64c959e7
Nitish Malhotra <nitishm@microsoft.com>
2022-02-25 19:49:05 +0000

    TLS for gRPC
    Signed-off-by: Nitish Malhotra <nitishm@microsoft.com>

---
+++ npm/pkg/transport/tls.go
@@ -0,0 +1,44 @@
+package transport
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+
+	"google.golang.org/grpc/credentials"
+)
+
+const (
+	serverCertPEMFilename = "tls.crt"
+	serverKeyPEMFilename  = "tls.key"
+	caCertPEMFilename     = "ca.crt"
+	path                  = "/etc/azure-npm/certs"
+)
+
+func serverTLSCreds() (credentials.TransportCredentials, error) {
+	certFilepath := path + "/" + serverCertPEMFilename
+	keyFilepath := path + "/" + serverKeyPEMFilename
+
+	return credentials.NewServerTLSFromFile(certFilepath, keyFilepath)
+}
+
+func clientTLSConfig() (*tls.Config, error) {
+	caCertFilepath := path + "/" + caCertPEMFilename
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile(caCertFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Create the credentials and return it
+	return &tls.Config{
+		RootCAs:            certPool,
+		InsecureSkipVerify: false,
+	}, nil
+}
